541432610e34773ce126e2ae63364e7388855482
ahmadferas1998 <aabuabdo1998@gmail.com>
2025-01-14 15:34:08 +0300

    fkpح

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build", // Specify the 'build' folder as the output directory
+  },
   plugins: [react()],
-})
+});
